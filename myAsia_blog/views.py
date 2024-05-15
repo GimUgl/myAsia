d6b9f6cbd6f4af5cb6901ed1ebecffa194acdff4
Jasurbek Sherzodov <jasurgamer67675@gmail.com>
2024-05-15 20:12:11 +0500

    views re4

--- myAsia_blog/views.py
+++ myAsia_blog/views.py
@@ -18,24 +18,6 @@ from lxml import etree
 def fb2_reader_view(request, book_pk):
     book = Books.objects.get(pk=book_pk)
 
-    # dir_url = os.path.join(BASE_DIR)
-    # dir_url1 = dir_url.split('\\')
-    # r_url1 = dir_url1[0] + '/' + dir_url1[1]
-    #
-    # file_url = os.path.join(book.files.url)
-    # file_url1 = file_url.split('/')
-    # r_url2 = file_url1[0] + '/' + file_url1[1] + '/' + file_url1[2] + '/'
-    #
-    # r_url = r_url1 + r_url2
-
-    # with open(r_url, encoding="utf-8") as file:
-    #     src = file.read()
-    #
-    # soup = BeautifulSoup(src, 'xml')
-    #
-    # find_body_section = soup.find('body').find_all('section')
-    # print(find_body_section)
-
     fb2_file = BASE_DIR + book.files.url
     txt_file = 'fb2.txt'
     tree = ET.parse(fb2_file)
